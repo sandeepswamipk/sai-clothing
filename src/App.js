6cf04dfae51a7eaf675339e1fb59a9023f455a77
sandeep Swami <sandeepswami1910@gmail.com>
2020-03-03 05:41:17 +0530

    header component created

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'; 
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <switch>
+      <Header />
+      <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-      </switch>
+      </Switch>
     </div>
   );
 }
