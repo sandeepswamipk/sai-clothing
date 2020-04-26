3ee9863bd9d8b76e92c1179bae9ea7e1a0874e87
sandeep Swami <sandeepswami1910@gmail.com>
2020-04-26 19:46:33 +0530

    redirect used

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Switch, Route } from 'react-router-dom';
+import {Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 
@@ -55,12 +55,25 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignOutPage} />
+          <Route 
+            exact 
+            path="/signin" 
+            render={() => 
+              this.props.currentUser ? (<Redirect to='/' />
+              ) : (
+                <SignInAndSignOutPage />
+              )
+            } 
+          />
         </Switch>
       </div>
     );
   }
 }
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
@@ -69,4 +82,4 @@ const mapDispatchToProps = dispatch => ({
   
 
 
-export default connect(null, mapDispatchToProps )(App);
+export default connect(mapStateToProps, mapDispatchToProps )(App);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,7 +1,9 @@
 
+import { UserActionTypes } from './user.types';
+
 export const setCurrentUser = function(user){
     return{
-        type: 'SET_CURRENT_USER',
+        type: UserActionTypes.SET_CURRENT_USER,
         payload: user
     }
 }
